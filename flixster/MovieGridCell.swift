980dbe9e4e9b53da2742c4f77cb6f84b3a572b42
Oscar Ruiz <Oscarruiz25341@gmail.com>
2021-09-13 22:34:21 -0700

    Updating for unit2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Oscar on 9/13/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
+
